b5e069599fb5b99e7533b5c1cf82a74c576a0432
Srinivas <varikutisrinivasreddy@gmail.com>
2024-09-18 13:23:17 +0530

    my first commit code

---
+++ src/BaseTestRunner/BaseTest.java
@@ -0,0 +1,13 @@
+package BaseTestRunner;
+
+public class BaseTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\sreddy353\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+        
+		System.out.println("Heel");
+		
+
+	}
+
+}
