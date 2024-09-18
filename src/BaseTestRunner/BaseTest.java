acb995c6335d1a881cd6a167c1b8421976ad0981
Srinivas <varikutisrinivasreddy@gmail.com>
2024-09-18 18:29:13 +0530

    code changed

--- src/BaseTestRunner/BaseTest.java
+++ src/BaseTestRunner/BaseTest.java
@@ -3,11 +3,9 @@ package BaseTestRunner;
 public class BaseTest {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\sreddy353\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
-        
-		System.out.println("Heel");
-		
-
+		System.out.println("hello Git");
+		System.out.println("Push the code to Git");
+	
 	}
 
 }
