e5c57c2eee17bc7ac65d283bb4dd0d690fb20a71
Srinivas <varikutisrinivasreddy@gmail.com>
2024-09-19 12:03:02 +0530

    new code changes

--- src/BaseTestRunner/BaseTest.java
+++ src/BaseTestRunner/BaseTest.java
@@ -5,6 +5,7 @@ public class BaseTest {
 	public static void main(String[] args) {
 		System.out.println("hello Git");
 		System.out.println("Push the code to Git");
+		System.out.println("Push the code latest to Git");
 	
 	}
 
